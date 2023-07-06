dffb656c5e04c2de19f9edbed382dafdf8034c56
Cytlan <cytlan@shibesoft.com>
2023-07-06 15:11:40 +0200

    Added read_places function

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ Name: Dick Tracy
 Date started: Yesterday
 Github URL: No
 """
+import csv # "you're not expected to use the csv module, but you're welcome to" - Don't mind if I do!
+
 def print_menu():
     print("""Menu:
 L - List places
@@ -12,14 +14,26 @@ A - Add new place
 M - Mark a place as visited
 Q - Quit""")
 
+def read_places(filename):
+    places = []
+    with open(filename, newline='') as csvfile:
+        placesReader = csv.reader(csvfile)
+        for row in placesReader:
+            places.append(row)
+    return places
+
 def main():
     print("Travel Tracker 1.0 - by Go Away")
+
+    # Read the list of places and parse it
+    places = read_places("places.csv")
+    print(places)
 
     # I will use the CORRECT method of implementing a menu
     # The way you've been taught how to implement a CLI menu is amature hour stuff
     while True:
         print_menu()
-        selection = ""
+        selection = input(">>> ").upper()
         if selection == "L":
             pass
         elif selection == "R":
